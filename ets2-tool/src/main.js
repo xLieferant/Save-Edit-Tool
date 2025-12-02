fad3c72714d606a386a11d3110e45311b2325014
Alexander K. <alexanderkruse04@icloud.com>
2025-12-02 19:47:39 +0100

    Update main.js

--- ets2-tool/src/main.js
+++ ets2-tool/src/main.js
@@ -83,6 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
     profileStatus.textContent = result;
 
     await updateAllDisplays();
+    await loadQuicksaveGame();
 
     const saveConfig = await invoke("read_save_config", {
       profilePath: selectedProfilePath,
