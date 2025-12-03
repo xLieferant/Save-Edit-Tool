7d3685e7110605451dd00c33c82f3f505dc02e7d
Alexander K. <alexanderkruse04@icloud.com>
2025-12-03 19:07:21 +0100

    Update trucks.rs

--- ets2-tool/src-tauri/src/commands/trucks.rs
+++ ets2-tool/src-tauri/src/commands/trucks.rs
@@ -8,11 +8,13 @@ use crate::models::trucks::ParsedTruck;
 #[command]
 pub async fn get_all_trucks(profile_path: String) -> Result<Vec<ParsedTruck>, String> {
     let game_sii_path = format!("{}/save/quicksave/game.sii", profile_path);
+    log!("game_sii_path wird geladen: {}", game_sii_path);
 
     let content = fs::read_to_string(&game_sii_path)
         .map_err(|e| format!("Fehler beim Lesen von game.sii: {}", e))?;
 
     let trucks = parse_trucks_from_sii(&content);
+        log!("parse_trucks_fronm_sii {}", parse_trucks_from_sii);
 
     Ok(trucks)
 }
