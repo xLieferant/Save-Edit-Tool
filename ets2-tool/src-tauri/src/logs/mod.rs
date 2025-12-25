e430da50e9f1e5e81068b4d292a81877e88096dd
Alexander K. <alexanderkruse04@icloud.com>
2025-12-26 00:17:49 +0100

    added logs/mod.rs

---
+++ ets2-tool/src-tauri/src/logs/mod.rs
@@ -0,0 +1,26 @@
+use chrono::Local;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+// Wichtig: Dieses Makro exportiert das globale log!-Makro
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {{
+        $crate::logs::write_log(format!($($arg)*));
+    }};
+}
+
+pub fn write_log(msg: String) {
+    let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S");
+    let entry = format!("[{}] {}\n", timestamp, msg);
+
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("ets2_tool.log")
+        .expect("Kann ets2_tool.log nicht öffnen");
+
+    let _ = file.write_all(entry.as_bytes());
+
+    println!("{}", entry);
+}
